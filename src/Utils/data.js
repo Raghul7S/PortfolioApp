7f715c5b952f06d7d4ae0ef368044f4616526d8d
Raghul7S <raghulvairavaraj@gmail.com>
2025-02-27 20:45:06 +0530

    Added new project detail

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,172 +1,181 @@
 export const SKILLS = [
-    {
-        title: "Frontend",
-        icon: "./assets/frontend-logo.png",
-        skills: [
-            { skill: "HTML5", percentage: "95%" },
-            { skill: "CSS{Sass/Scss}", percentage: "90%" },
-            { skill: "Javascript", percentage: "50%" },
-            { skill: "React.js", percentage: "60%" },
-            { skill: "React Native", percentage: "60%" }
-        ],
-    },
-    {
-        title: "Backend",
-        icon: "./assets/backend-logo.png",
-        skills: [
-            { skill: "Node.js", percentage: "25%" },
-            { skill: "MongoDB", percentage: "50%" },
-        ],
-    },
-    {
-        title: "Tools",
-        icon: "./assets/tools-icon.png",
-        skills: [
-            { skill: "Git & Github", percentage: "70%" },
-            { skill: "Visual Studio Code", percentage: "90%" },
-            { skill: "Responsive Design", percentage: "80%" },
-            { skill: "Canva", percentage: "80%" },
-        ],
-    },
-    {
-        title: "Soft Skills",
-        icon: "./assets/soft-skills.png",
-        skills: [
-            { skill: "Problem Solving", percentage: "80%" },
-            { skill: "Attention To Detail", percentage: "80%" },
-            { skill: "Collaboration", percentage: "85%" },
-            { skill: "Self Learning", percentage: "80%" },
-            { skill: "Logical Thinking", percentage: "80%" }
-        ],
-    },
+  {
+    title: "Frontend",
+    icon: "./assets/frontend-logo.png",
+    skills: [
+      { skill: "HTML5", percentage: "95%" },
+      { skill: "CSS{Sass/Scss}", percentage: "90%" },
+      { skill: "Javascript", percentage: "50%" },
+      { skill: "React.js", percentage: "60%" },
+      { skill: "React Native", percentage: "60%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./assets/backend-logo.png",
+    skills: [
+      { skill: "Node.js", percentage: "25%" },
+      { skill: "MongoDB", percentage: "50%" },
+    ],
+  },
+  {
+    title: "Tools",
+    icon: "./assets/tools-icon.png",
+    skills: [
+      { skill: "Git & Github", percentage: "70%" },
+      { skill: "Visual Studio Code", percentage: "90%" },
+      { skill: "Responsive Design", percentage: "80%" },
+      { skill: "Canva", percentage: "80%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./assets/soft-skills.png",
+    skills: [
+      { skill: "Problem Solving", percentage: "80%" },
+      { skill: "Attention To Detail", percentage: "80%" },
+      { skill: "Collaboration", percentage: "85%" },
+      { skill: "Self Learning", percentage: "80%" },
+      { skill: "Logical Thinking", percentage: "80%" },
+    ],
+  },
 ];
 
 export const PROJECT_DETAIL = [
+  {
+    title: "1. ToDo List Mobile Application",
+    subject: "Simple ToDo list app",
+    responsibilities: [
+      "Developed a ToDo list app as mobile application.",
+      "Easily create, modify, or remove tasks with a simple tap.",
+      "Implemented AsyncStorage to save tasks locally, ensuring data is retained even after closing the app.",
+      "Integrated theme switching for both light and dark modes.",
+    ],
+    link: "https://github.com/Raghul7S/Todo-MobileApp",
+  },
+  {
+    title: "2. Film Discovery Site",
+    subject: "Movie exploring app",
+    responsibilities: [
+      "Developed a Movie Search and Discovery App using React and TMDB API.",
+      "Integrated the TMDB API to fetch real-time movie data, including titles, posters, and descriptions.",
+      "Added a responsive design to ensure seamless functionality across desktop and mobile devices.",
+      "Implemented a search feature to allow users to find movies by title or keyword.",
+      "Added a favorites feature to allow users to save and manage their favorite movies.",
+    ],
+    link: "https://raghul7s.github.io/MovieExplorer/",
+  },
+  {
+    title: "3. Code Pen Editor Clone",
+    subject: "Simple Code Pen Editor",
+    responsibilities: [
+      "Developed a CodePen clone using React.",
+      "Integrated the Codemirror package for accessing HTML, CSS, and JavaScript functionalities.",
+      "Utilized local storage to maintain code state across page reloads.",
+      "Demonstrated proficiency in React, Codemirror, and front-end development skills.",
+      "Provided a platform for users to experiment with code.",
+    ],
+    link: "https://raghul7s.github.io/CodePen/",
+  },
 
-    {
-        title: "1. ToDo List Mobile Application",
-        subject: "Simple ToDo list app",
-        responsibilities: [
-            "Developed a ToDo list app as mobile application.",
-            "Easily create, modify, or remove tasks with a simple tap.",
-            "Implemented AsyncStorage to save tasks locally, ensuring data is retained even after closing the app.",
-            "Integrated theme switching for both light and dark modes."
-        ],
-        link: "https://github.com/Raghul7S/Todo-MobileApp"
-    },
-    {
-        title: "2. Code Pen Editor Clone",
-        subject: "Simple Code Pen Editor",
-        responsibilities: [
-            "Developed a CodePen clone using React.",
-            "Integrated the Codemirror package for accessing HTML, CSS, and JavaScript functionalities.",
-            "Utilized local storage to maintain code state across page reloads.",
-            "Demonstrated proficiency in React, Codemirror, and front-end development skills.",
-            "Provided a platform for users to experiment with code.",
-        ],
-        link: "https://raghul7s.github.io/CodePen/",
-    },
+  {
+    title: "4. Social Media Post App",
+    subject: "An app using CRUD operation and API",
+    responsibilities: [
+      "Developed a social media post application using React.",
+      "Implemented CRUD (Create, Read, Update, Delete) functionality for managing posts.",
+      "Integrated a local API to store and retrieve post data.",
+      "Focused on creating a responsive and user-friendly interface.",
+      "Project showcases proficiency in React, API integration, and front-end development skills.",
+    ],
+    link: "https://github.com/Raghul7S/Social-Media-Post-App----React",
+  },
 
-    {
-        title: "3. Social Media Post App",
-        subject: "An app using CRUD operation and API",
-        responsibilities: [
-            "Developed a social media post application using React.",
-            "Implemented CRUD (Create, Read, Update, Delete) functionality for managing posts.",
-            "Integrated a local API to store and retrieve post data.",
-            "Focused on creating a responsive and user-friendly interface.",
-            "Project showcases proficiency in React, API integration, and front-end development skills.",
-        ],
-        link: "https://github.com/Raghul7S/Social-Media-Post-App----React",
-    },
+  {
+    title: "5. Trip App",
+    subject: "An Trip booking app UI",
+    responsibilities: [
+      "Developed a travel trip app UI using React.",
+      "Ensured the UI is fully responsive, adapting to various screen sizes and devices.",
+      "Designed the UI with a focus on user-friendliness and ease of navigation.",
+      "Included features to enhance the user experience, such as intuitive controls and clear information presentation.",
+      "Project showcases proficiency in React, UI design, and responsive web development skills.",
+    ],
+    link: "https://raghul7s.github.io/Trip-Website/",
+  },
 
-    {
-        title: "4. Trip App",
-        subject: "An Trip booking app UI",
-        responsibilities: [
-            "Developed a travel trip app UI using React.",
-            "Ensured the UI is fully responsive, adapting to various screen sizes and devices.",
-            "Designed the UI with a focus on user-friendliness and ease of navigation.",
-            "Included features to enhance the user experience, such as intuitive controls and clear information presentation.",
-            "Project showcases proficiency in React, UI design, and responsive web development skills.",
-        ],
-        link: "https://raghul7s.github.io/Trip-Website/",
-    },
+  {
+    title: "6. Flappy Bird Game",
+    subject: "Developed using JS",
+    responsibilities: [
+      "Cloned the popular Flappy Bird game using JavaScript.",
+      "Ensured the game is responsive and user-friendly.",
+      "Added a scoreboard to track the player's score.",
+      "Included a restart button for restarting the game.",
+      "Implemented game mechanics faithful to the original Flappy Bird game.",
+    ],
+    link: "https://github.com/Raghul7S/Flappy-Bird-Game----JS",
+  },
 
-    {
-        title: "5. Flappy Bird Game",
-        subject: "Developed using JS",
-        responsibilities: [
-            "Cloned the popular Flappy Bird game using JavaScript.",
-            "Ensured the game is responsive and user-friendly.",
-            "Added a scoreboard to track the player's score.",
-            "Included a restart button for restarting the game.",
-            "Implemented game mechanics faithful to the original Flappy Bird game.",
-        ],
-        link: "https://github.com/Raghul7S/Flappy-Bird-Game----JS",
-    },
+  {
+    title: "7. Weather App",
+    subject: "Developed using Js",
+    responsibilities: [
+      "Created a Weather App using HTML, CSS and JavaScript.",
+      "User can enter the name of a city to fetch weather data.",
+      "Triggers the weather data fetch for the specified city.",
+      "Shows the city name and weather description.",
+      "Alerts the user if the city is not found or if there is an error fetching data.",
+    ],
+    link: "https://github.com/Raghul7S/WeatherApp",
+  },
 
-    {
-        title: "6. Weather App",
-        subject: "Developed using Js",
-        responsibilities: [
-            "Created a Weather App using HTML, CSS and JavaScript.",
-            "User can enter the name of a city to fetch weather data.",
-            "Triggers the weather data fetch for the specified city.",
-            "Shows the city name and weather description.",
-            "Alerts the user if the city is not found or if there is an error fetching data.",
-        ],
-        link: "https://github.com/Raghul7S/WeatherApp",
-    },
+  {
+    title: "8. Simple Calculator",
+    subject: "Basic Calculator using JS",
+    responsibilities: [
+      "Created a basic calculator using HTML, CSS, and JavaScript.",
+      "Implemented arithmetic operations such as addition, subtraction, multiplication, and division.",
+      "Designed the calculator's interface with HTML and CSS for a visually appealing and intuitive user experience.",
+      "Used JavaScript to handle user input and perform the arithmetic calculations.",
+      "Ensured the calculator is functional and responsive on various devices.",
+    ],
+    link: "https://github.com/Raghul7S/Simple-Calculator",
+  },
 
-    {
-        title: "7. Simple Calculator",
-        subject: "Basic Calculator using JS",
-        responsibilities: [
-            "Created a basic calculator using HTML, CSS, and JavaScript.",
-            "Implemented arithmetic operations such as addition, subtraction, multiplication, and division.",
-            "Designed the calculator's interface with HTML and CSS for a visually appealing and intuitive user experience.",
-            "Used JavaScript to handle user input and perform the arithmetic calculations.",
-            "Ensured the calculator is functional and responsive on various devices.",
-        ],
-        link: "https://github.com/Raghul7S/Simple-Calculator",
-    },
-
-    {
-        title: "8. Online Academy",
-        subject: "User Interface of an Academy website",
-        responsibilities: [
-            "Developed an online software academy website using Bootstrap.",
-            "Focused on designing the user interface (UI) using Bootstrap's components and utilities.",
-            "Ensured the website is fully responsive, adapting seamlessly to various screen sizes and devices.",
-            "Designed the UI to be user-friendly, intuitive, and visually appealing.",
-            "Project showcases proficiency in Bootstrap for responsive web design and UI development.",
-        ],
-        link: "https://github.com/Raghul7S/Online-Academy----Bootstrap",
-    },
+  {
+    title: "9. Online Academy",
+    subject: "User Interface of an Academy website",
+    responsibilities: [
+      "Developed an online software academy website using Bootstrap.",
+      "Focused on designing the user interface (UI) using Bootstrap's components and utilities.",
+      "Ensured the website is fully responsive, adapting seamlessly to various screen sizes and devices.",
+      "Designed the UI to be user-friendly, intuitive, and visually appealing.",
+      "Project showcases proficiency in Bootstrap for responsive web design and UI development.",
+    ],
+    link: "https://github.com/Raghul7S/Online-Academy----Bootstrap",
+  },
 ];
 
 export const WORK_EXPERIENCE = [
-
-    {
-        title: "1. Grafin IT (June 2024 - Oct 2024)",
-        subject: "Front-end Developer in Web Platform and Mobile Application",
-        responsibilities: [
-            "Worked on an e-commerce project, developing user interfaces for web applications.",
-            "Contributed to front-end development for mobile applications, ensuring responsiveness and user-friendly designs.",
-            "Gained experience in building dynamic and interactive user interfaces for both web and mobile platforms.",
-            "Collaborated with the UI/UX team to incorporate user-centered design principles, resulting in an aesthetically appealing and responsive interface for the project.",
-        ],
-    },
-    {
-        title: "2. HandBuiltApps (Currently working here)",
-        subject: "Mobile Application Developer",
-        responsibilities: [
-            "Learned and implemented React Native and Redux for mobile application development.",
-            "Gained experience in using Xcode for iOS development and managing Android builds.",
-            "Maintained clean and structured code for scalability and efficiency.",
-            "Debugged and resolved issues across Android and iOS platforms to ensure app stability.",
-        ],
-    }
-
+  {
+    title: "1. Grafin IT (June 2024 - Oct 2024)",
+    subject: "Front-end Developer in Web Platform and Mobile Application",
+    responsibilities: [
+      "Worked on an e-commerce project, developing user interfaces for web applications.",
+      "Contributed to front-end development for mobile applications, ensuring responsiveness and user-friendly designs.",
+      "Gained experience in building dynamic and interactive user interfaces for both web and mobile platforms.",
+      "Collaborated with the UI/UX team to incorporate user-centered design principles, resulting in an aesthetically appealing and responsive interface for the project.",
+    ],
+  },
+  {
+    title: "2. HandBuiltApps (Currently working here)",
+    subject: "Mobile Application Developer",
+    responsibilities: [
+      "Learned and implemented React Native and Redux for mobile application development.",
+      "Gained experience in using Xcode for iOS development and managing Android builds.",
+      "Maintained clean and structured code for scalability and efficiency.",
+      "Debugged and resolved issues across Android and iOS platforms to ensure app stability.",
+    ],
+  },
 ];
