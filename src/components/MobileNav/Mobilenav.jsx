7291cc2d96bf69d78e1a54c0d9a671a0dd18f48f
Raghul7S <raghulvairavaraj@gmail.com>
2025-03-08 11:23:09 +0530

    Resume Updated

--- src/components/MobileNav/Mobilenav.jsx
+++ src/components/MobileNav/Mobilenav.jsx
@@ -1,47 +1,61 @@
-import React from 'react'
-import './Mobilenav.css'
+import React from "react";
+import "./Mobilenav.css";
 
-export const Mobilenav = ({isOpen, toggleMenu}) => {
-
-    const redirectToResume = () => {
-        window.open('https://drive.google.com/file/d/16jVnJbSZt5zYLj5dIldA0S64mqq7NpeS/view?usp=sharing');
-      };
+export const Mobilenav = ({ isOpen, toggleMenu }) => {
+  const redirectToResume = () => {
+    window.open(
+      "https://drive.google.com/file/d/1tLOqXt3BCNb7alsVL7EfhC2-WVCSSu81/view?usp=sharing"
+    );
+  };
 
   return (
     <>
-    <div className={`mobile-menu  ${isOpen ? "active":""}`} onClick={toggleMenu}>
+      <div
+        className={`mobile-menu  ${isOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="mobile-menu-container">
+          <h1 className="logo" style={{ color: "white" }}>
+            RAGHUL V
+          </h1>
 
-        <div className='mobile-menu-container'>
-            <h1 className='logo' style={{color:"white"}}>RAGHUL V</h1>
+          <ul>
+            <li>
+              <a className="menu-item" href="#hero">
+                Home
+              </a>
+            </li>
 
-            <ul>
-                <li>
-                    <a className='menu-item' href='#hero'>Home</a>
-                </li>
+            <li>
+              <a className="menu-item" href="#skills">
+                Skills
+              </a>
+            </li>
 
-                <li>
-                    <a className='menu-item' href='#skills'>Skills</a>
-                </li>
+            <li>
+              <a className="menu-item" href="#experience">
+                Experience
+              </a>
+            </li>
 
-                <li>
-                    <a className='menu-item' href='#experience'>Experience</a>
-                </li>
+            <li>
+              <a className="menu-item" href="#projects">
+                Projects
+              </a>
+            </li>
 
-                <li>
-                    <a className='menu-item' href='#projects'>Projects</a>
-                </li>
+            <li>
+              <a className="menu-item" href="#contact">
+                Contact Me
+              </a>
+            </li>
 
-                <li>
-                    <a className='menu-item' href='#contact'>Contact Me</a>
-                </li>
-
-                <button className='contact-btn' onClick={redirectToResume}>
-                   Resume
-                </button>
-                
-            </ul>
+            <button className="contact-btn" onClick={redirectToResume}>
+              Resume
+            </button>
+          </ul>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 import { TbGridDots } from "react-icons/tb";
 import { IoMdClose } from "react-icons/io";
-import { Mobilenav } from '../MobileNav/Mobilenav';
+import { Mobilenav } from "../MobileNav/Mobilenav";
 
 const Navbar = () => {
-
   const [openMenu, setOpenMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -13,49 +12,63 @@ const Navbar = () => {
   };
 
   const redirectToResume = () => {
-    window.open('https://drive.google.com/file/d/16jVnJbSZt5zYLj5dIldA0S64mqq7NpeS/view?usp=sharing');
+    window.open(
+      "https://drive.google.com/file/d/1tLOqXt3BCNb7alsVL7EfhC2-WVCSSu81/view?usp=sharing"
+    );
   };
 
-  
   return (
     <>
       <Mobilenav isOpen={openMenu} toggleMenu={toggleMenu} />
 
-      <nav className='nav-wrapper'>
-        <div className='nav-content'>
-          <h1 className='logo' style={{color: "white"}}>RAGHUL V</h1>
+      <nav className="nav-wrapper">
+        <div className="nav-content">
+          <h1 className="logo" style={{ color: "white" }}>
+            RAGHUL V
+          </h1>
 
           <ul>
             <li>
-              <a className='menu-item' href='#hero'>Home</a>
+              <a className="menu-item" href="#hero">
+                Home
+              </a>
             </li>
             <li>
-              <a className='menu-item' href='#skills'>Skills</a>
+              <a className="menu-item" href="#skills">
+                Skills
+              </a>
             </li>
             <li>
-              <a className='menu-item' href='#experience'>Experience</a>
+              <a className="menu-item" href="#experience">
+                Experience
+              </a>
             </li>
             <li>
-              <a className='menu-item' href='#projects'>Projects</a>
+              <a className="menu-item" href="#projects">
+                Projects
+              </a>
             </li>
             <li>
-              <a className='menu-item' href='#contact'>Contact Me</a>
+              <a className="menu-item" href="#contact">
+                Contact Me
+              </a>
             </li>
 
-            <button className='contact-btn'  onClick={redirectToResume}>
-               Resume
+            <button className="contact-btn" onClick={redirectToResume}>
+              Resume
             </button>
-            
           </ul>
 
-          <button className='menu-btn' onClick={toggleMenu}>
-            {openMenu ? <IoMdClose style={{ fontSize: "1.8rem" }} /> : <TbGridDots style={{ fontSize: "1.8rem" }} />}
+          <button className="menu-btn" onClick={toggleMenu}>
+            {openMenu ? (
+              <IoMdClose style={{ fontSize: "1.8rem" }} />
+            ) : (
+              <TbGridDots style={{ fontSize: "1.8rem" }} />
+            )}
           </button>
-
         </div>
       </nav>
     </>
-
   );
 };
 
