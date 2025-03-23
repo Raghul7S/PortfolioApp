4943dc8cb778cb24695e30ae4633c95292222adf
Raghul7S <raghulvairavaraj@gmail.com>
2025-03-23 11:01:32 +0530

    Icon animation

--- src/components/Projects/Projectcard/Projectcard.jsx
+++ src/components/Projects/Projectcard/Projectcard.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import './Projectcard.css'
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import "./Projectcard.css";
+import { FaGithub } from "react-icons/fa";
 
-
-const Projectcard = ({details}) => {
+const Projectcard = ({ details }) => {
   return (
-    <div className='project-card'>
-      <div className='title-icon'>
+    <div className="project-card">
+      <div className="title-icon">
         <h6>{details.title}</h6>
-        <a href={details.link} target="_blank" rel="noopener noreferrer" className="github-link">
-           <FaGithub />
+        <a
+          href={details.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          <FaGithub className="github-icon" />
         </a>
-      </div>   
-        <div className="project-subject">
-          {details.subject}
-        </div>
-       
-        <ul>
-            {details.responsibilities.map((item)=>(
-                <li key={item}>{item}</li>
-            ))}
-        </ul>
-    </div>
-  )
-}
+      </div>
+      <div className="project-subject">{details.subject}</div>
 
-export default Projectcard
+      <ul>
+        {details.responsibilities.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Projectcard;
