a5115b6e84b1647d0635af35c6dc24bc8e16503a
absmani16 <absmani101@gmail.com>
2025-08-05 17:25:10 +0530

    Animation added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Skills from './components/Skills/Skills';
+import Experience from './components/Experience/Experience';
+import Projects from './components/Project/Projects';
 import Contact from './components/Contactme/Contact';
 import Footer from './components/Footer/Footer';
-import Experience from './components/Experience/Experience';
+
 import './App.css'
-import Projects from './components/Project/Projects';
 
 function App() {
+
+  useEffect(()=> {
+    Aos.init({
+      duration: 1000,
+      once: true
+    });
+  }, []);
+
   return (
    <div>
       <Navbar />
